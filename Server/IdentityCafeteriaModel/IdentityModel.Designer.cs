8ffaa07238d131b871530133fe4ff1c2a7d08c11
sinful_johnny <megaVNN321123@gmail.com>
2024-12-14 18:58:23 +0700

    build: Access Control Metadata

--- Server/IdentityCafeteriaModel/IdentityModel.Designer.cs
+++ Server/IdentityCafeteriaModel/IdentityModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\Github\Cafeteria-Management\SampleProjects\Server\IdentityCafeteriaModel\IdentityModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Download\Clone\cafeteria-management-be\Server\IdentityCafeteriaModel\IdentityModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
